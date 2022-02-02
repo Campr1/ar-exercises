00d1fae6ebc6e4b948c0a22f4ac5077ac9348ef8
Rishard C <rishard.c@hotmail.com>
2022-02-01 20:50:42 -0500

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yalestown", annual_revenue: 43000, mens_apparel: true, womens_apparel: true)
+
+# fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable
+
+@mens_stores = Store.where("mens_apparel = true")
+
+# Loop through each of these stores and output their name and annual revenue on each line.
+
+@mens_stores.each do |store|
+  puts "name: #{store.name}, annual revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+# Repeat
+@womens_stores.each do |store|
+  puts "name: #{store.name}, annual revenue: #{store.annual_revenue}"
+end
